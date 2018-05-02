1684641f9a359a44bfde35f0df4366ac04c15c46
John Park <ohrak22@gmail.com>
2018-05-02 09:55:25 +0700

    Add Character.

---
+++ Client/MyJRPG_UnityProject/Assets/Scripts/Tools/CharacterTool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterTool : MonoBehaviour {
+
+	public CharAniControl charAni;
+
+	public void OnIdle()
+	{
+		charAni.PlayAni(CharAniControl.State.BattleIdle);
+	}
+
+	public void OnAttack()
+	{
+		charAni.PlayAni(CharAniControl.State.Attack);
+	}
+
+	public void OnHit()
+	{
+		charAni.PlayAni(CharAniControl.State.Hit);
+	}
+
+}
